a38737ec557758dc0bb9c56450633704c2999f05
joi0611 <howeyglockner@gmail.com>
2026-07-17 16:02:33 +0800

    Add files via upload

---
+++ zaytun/training/grammar/official-exams-grammar.js
@@ -0,0 +1,63 @@
+addExamArticle("officialXinjiang2025Grammar", {
+  title: "2025 新疆中考真题",
+  practiceTitle: "商鞅变法与立木取信",
+  source: "2025年 · 新疆维吾尔自治区、新疆生产建设兵团 · 初中学业水平考试英语真题",
+  group: "中考练习篇目",
+  startNo: 74,
+  official: true,
+  year: 2025,
+  region: "新疆维吾尔自治区、新疆生产建设兵团",
+  examName: "初中学业水平考试英语真题",
+  questions: [
+    q(74, "(success)", ["successful"], ["success", "successful", "successfully", "succeed"], "形容词与副词", "形容词修饰名词", "changes", "空后是名词 changes，需要形容词修饰名词，所以 success 变为 successful。"),
+    q(75, "(plan)", ["plans"], ["plan", "plans", "planned", "planning"], "名词", "可数名词变复数", "a set of", "a set of 后接可数名词复数，表示“一套计划”，所以用 plans。"),
+    vq(76, "(accept)", ["accepted"], ["accept", "accepted", "was accepted", "were accepted"], "有", "非谓语", "过去分词作表语", "were not easily", "空前已有 were，后面用过去分词 accepted 构成被动含义：计划不容易被接受。"),
+    nq(77, ["up"], ["up", "out", "on", "in"], "副词", "come up with", "came / with an idea", "come up with an idea 表示“想出一个主意”，所以填 up。"),
+    vq(78, "(receive)", ["receive"], ["receive", "receives", "received", "to receive"], "没有", "谓语", "情态动词后动词原形", "would", "would 后接动词原形，所以用 receive。"),
+    nq(79, ["But"], ["But", "So", "And", "Because"], "连词", "转折连词", "The task was so easy / nobody believed", "前句说任务很容易，后句说没人相信是真的，前后为转折关系，用 But。"),
+    q(80, "(final)", ["finally"], ["final", "finally", "finals", "finalist"], "形容词与副词", "形容词变副词", "a man wanted to have a try", "修饰整个句子，表示“最终”，用副词 finally。"),
+    nq(81, ["To"], ["To", "For", "At", "With"], "介词", "to one's surprise", "his surprise", "to one's surprise 表示“令某人惊讶的是”，句首首字母大写为 To。"),
+    q(82, "(he)", ["his"], ["he", "him", "his", "himself"], "代词", "形容词性和名词性物主代词", "word", "空后是名词 word，需要形容词性物主代词 his。"),
+    vq(83, "(follow)", ["followed"], ["follow", "followed", "following", "follows"], "没有", "谓语", "时态", "Then people", "文章叙述战国时期发生的历史事件，整体为一般过去时，所以用 followed。"),
+    vq(84, "(become)", ["became"], ["become", "becomes", "became", "becoming"], "没有", "谓语", "时态", "With his efforts", "文章讲过去的历史结果，谓语动词用一般过去时 became。")
+  ],
+  paragraphs: [
+    "During the Warring States Period, many states made changes to become stronger. One of the most [[74]] changes was created by Shang Yang in Qin.",
+    "Shang Yang worked out a set of [[75]]. At first, these plans were not easily [[76]] because people didn't believe in him.",
+    "To solve this problem, Shang Yang came [[77]] with an idea. He ordered his men to put a thin wooden pole at the south gate. Then he said anyone who took the pole to the north gate would [[78]] 10 gold pieces. The task was so easy. [[79]] nobody believed it was true. They all thought Shang Yang was joking. After seeing that, Shang Yang said he would offer 50 gold pieces to anyone who did it.",
+    "And [[80]], a man wanted to have a try. He put the pole on his shoulder and walked to the north gate. [[81]] his surprise, Shang Yang paid him 50 gold pieces.",
+    "The news spread across the capital. Soon, people were saying Shang Yang was a man of [[82]] word. Then people [[83]] him without question. With his efforts, Qin [[84]] the strongest of all the states."
+  ]
+});
+
+addExamArticle("officialXinjiang2024Grammar", {
+  title: "2024 新疆中考真题",
+  practiceTitle: "山东奶奶的钩针艺术",
+  source: "2024年 · 新疆维吾尔自治区 · 初中学业水平考试英语真题",
+  group: "中考练习篇目",
+  startNo: 54,
+  official: true,
+  year: 2024,
+  region: "新疆维吾尔自治区",
+  examName: "初中学业水平考试英语真题",
+  questions: [
+    nq(54, ["for"], ["for", "since", "during", "in"], "介词", "for + 时间段", "about 50 years", "about 50 years 是一段时间，前面用 for，表示持续约 50 年。"),
+    vq(55, "(finish)", ["to finish"], ["finish", "finishing", "to finish", "finished"], "有", "非谓语", "it takes sb. time to do sth.", "It often takes me one week", "It takes sb. some time to do sth. 表示“做某事花费某人多长时间”，所以用 to finish。"),
+    nq(56, ["but"], ["and", "but", "so", "because"], "连词", "转折连词", "takes me one week / never feel tired", "前句说花费时间长，后句说从不厌倦，前后转折，用 but。"),
+    q(57, "(kind)", ["kinds"], ["kind", "kinds", "kindly", "kindness"], "名词", "可数名词变复数", "different", "different 后接可数名词复数，different kinds of 表示“不同种类的”。"),
+    vq(58, "(see)", ["see"], ["see", "sees", "saw", "seeing"], "没有", "谓语", "情态动词后动词原形", "can", "情态动词 can 后接动词原形 see。"),
+    q(59, "(kid)", ["kids"], ["kid", "kids", "kid's", "kidding"], "名词", "可数名词变复数", "as toys for", "作为孩子们的玩具，kid 表类别用复数 kids。"),
+    vq(60, "(be)", ["is"], ["is", "are", "was", "were"], "没有", "谓语", "主谓一致", "The old woman", "全文介绍人物现状，用一般现在时；主语 The old woman 是单数，用 is。"),
+    q(61, "(she)", ["her"], ["she", "her", "hers", "herself"], "代词", "形容词性和名词性物主代词", "free time", "in one's free time 表示“在某人的空闲时间”，空后有名词 time，用 her。"),
+    q(62, "(much)", ["more"], ["much", "more", "most", "many"], "形容词与副词", "比较级与最高级", "than before", "than before 是比较级线索，much 的比较级是 more。"),
+    nq(63, ["a"], ["a", "an", "the", "/"], "冠词", "不定冠词", "factory of her own", "factory 是可数名词单数，第一次泛指“一个工厂”，且以辅音音素开头，用 a。"),
+    vq(64, "(do)", ["doing"], ["do", "does", "doing", "did"], "没有", "谓语", "现在进行时", "Now she is", "Now 和 is 提示现在进行时，结构 be doing，所以填 doing。")
+  ],
+  paragraphs: [
+    "A 68-year-old woman from Shandong Province now teaches crocheting online and offline. She has made crochet artworks [[54]] about 50 years.",
+    "“It often takes me one week [[55]] a crochet artwork [[56]] I never feel tired of it,” the old woman said.",
+    "There are different [[57]] of colorful products in the old woman’s workshop. As you can [[58]], some of them are traditional and some of them are fashionable. They can be used as decorations for homes or as toys for [[59]].",
+    "The old woman [[60]] warm-hearted. In [[61]] free time, she teaches women villagers to make crochet artworks. She helps them to make [[62]] money than before.",
+    "The old woman has [[63]] factory of her own. Now she is [[64]] her best to develop some new products."
+  ]
+});
